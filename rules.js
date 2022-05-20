b1c0cfe2fb80466351b379f793a679821516c0b7
BenjaminTruckenbrod <btrucker94@gmail.com>
2022-05-20 10:24:46 -0400

    Finish the ui for rps

--- rules.js
+++ rules.js
@@ -3,68 +3,94 @@ function getRandomInt(max) {
   }
 
 function computerPlay(){
-    options = ['rock', 'paper', 'scissors'];
-    pick = getRandomInt(3);
+    let options = ['rock', 'paper', 'scissors'];
+    let pick = getRandomInt(3);
     return options[pick]
 }
 
 function playRound(playerSelection , computerSelection){    
     
-    if (playerSelection == "rock" && computerSelection == 'paper')
+    if (playerSelection == "rock" && computerSelection == 'paper'){
         return 'You Lose! Paper beats Rock';
-    if (playerSelection == "rock" && computerSelection == 'scissors')
+    }    
+    if (playerSelection == "rock" && computerSelection == 'scissors'){
         return 'You Win! Rock beats Paper';
-    if (playerSelection == "rock" && computerSelection == 'rock')
+    }
+    if (playerSelection == "rock" && computerSelection == 'rock'){
         return 'It\'s a tie!';
-    if (playerSelection == "paper" && computerSelection == 'paper')
+    }
+    if (playerSelection == "paper" && computerSelection == 'paper'){
         return 'It\'s a tie!';
-    if (playerSelection == "paper" && computerSelection == 'scissors')
+    }
+    if (playerSelection == "paper" && computerSelection == 'scissors'){
         return 'You Lose! Scissors beats Paper';
-    if (playerSelection == "paper" && computerSelection == 'rock')
+    }
+    if (playerSelection == "paper" && computerSelection == 'rock'){
         return 'You Win! Paper beats Rock';
-    if (playerSelection == "scissors" && computerSelection == 'paper')
+    }    
+    if (playerSelection == "scissors" && computerSelection == 'paper'){
         return 'You Win! Scissors beats Paper';
-    if (playerSelection == "scissors" && computerSelection == 'scissors')
+    }
+    if (playerSelection == "scissors" && computerSelection == 'scissors'){
         return 'It\'s a tie!';
-    if (playerSelection == "scissors" && computerSelection == 'rock')
+    }
+    if (playerSelection == "scissors" && computerSelection == 'rock'){
         return 'You Lose! Rock beats Scissors';
+    }
 
 }
 
-//console.log(computerPlay());
-//playerSelection = prompt('What do you choose?').toLowerCase();
-//computerSelection = computerPlay();
-//console.log(playRound(playerSelection, computerSelection));
-
-function game(){
-    scoreA=0;
-    scoreB=0;
-
-    //for (let i=0; i<5; i++) {
-        //computerSelection = computerPlay();
-        
-        //result = playRound(playerSelection, computerSelection);
-        //if (result.charAt(4) == 'L')
-            //scoreB++;
-        //if (result.charAt(4) == 'W')
-            //scoreA++;
-        //console.log(result);
-
-    //}
-    if (scoreA>scoreB)
-        return "You won the game!";
-    return "You lost the game!";
-}
-
-
+let scorePlayer=0;
+let scoreComp=0;
 
 const buttons = document.querySelectorAll('button');
+
 buttons.forEach((button) => {
 
-    // and for each one we add a 'click' listener
     button.addEventListener('click', () => {
-      alert(button.id);
       compPlay = computerPlay();
-      playRound(button.id, compPlay);
+      result = playRound(button.id, compPlay);
+      if (result.charAt(4) == 'W')
+        scorePlayer++;
+      if (result.charAt(4) == 'L')
+        scoreComp++;
+      
+      sPlayer.textContent="Player Score: " + scorePlayer;
+      sComp.textContent="Computer Score: " + scoreComp ;
+      
+      if (scoreComp >= 5){
+        p.textContent="The computer has won the game";
+        scorePlayer=0;
+        scoreComp=0;
+      }
+      else if (scorePlayer >= 5){
+        p.textContent="You won the game";  
+        scorePlayer=0;
+        scoreComp=0;
+      }
+      else {
+        p.textContent=result;
+        }
+      
     });
   });
+
+const container=document.querySelector('#container');
+
+const p = document.createElement('p');
+p.classList.add('result')
+p.style.color='blue';
+container.appendChild(p);
+
+const sPlayer = document.createElement('p');
+sPlayer.classList.add('score')
+sPlayer.style.color='green';
+container.appendChild(sPlayer);
+
+const sComp = document.createElement('p');
+sComp.classList.add('score')
+sComp.style.color='green';
+container.appendChild(sComp);
+
+
+
